79cc5d741774fe085ee46d65431de37eca110bee
Nial Dennison <nial_dennison@rapid7.com>
2023-02-10 10:46:46 +0000

    remove additional deps

--- packages/ui/webpack.config.js
+++ packages/ui/webpack.config.js
@@ -16,28 +16,6 @@ module.exports = {
   ],
   module: {
     rules: [
-      // {
-      //   test: /\.m?jsx$/,
-      //   exclude: /(node_modules)/,
-      //   use: {
-      //     loader: "babel-loader",
-      //     options: {
-      //       presets: [
-      //         "@babel/preset-env",
-      //         "@babel/preset-react",
-      //         "@babel/preset-typescript",
-      //       ],
-      //       "plugins": [
-      //         "@babel/proposal-class-properties",
-      //         ["@babel/plugin-transform-runtime",
-      //           {
-      //             "regenerator": true
-      //           }
-      //         ]
-      //       ],
-      //     },
-      //   },
-      // },
       { test: /\.tsx?$/, use: {loader: "ts-loader"} },
     ],
   },
